e1befff10a29adeae3d4eb90809ece5e104f14ab
邓小宇 <kiwi.deng@71360.com>
2020-07-09 15:37:05 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # saas平台权限管理系统
-
 ## 软件要求
 mysql8.0以上
+## 技术框架
+   |spring boot| shiro|jdk|
+   |--------| -----:|-----:|
+   |2.3.1.RELEASE|1.4.0|1.8|
+## 使用步骤
+ **步骤1** 通过doc/sql/auth.sql来初始化表结构
+ ### 默认用户-角色-权限关系表
+ |用户|角色|权限|
+ |---- |----|----|
+ |admin|超级管理员|select;save;delete;update|
+ |jack|管理员|select;save;update|
+ |marry|普通用户|select|
+ **步骤2** : 
+    访问swagger http://localhost:8090/swagger-ui.html
+    ![avatar](doc/pic/swagger.png)   
+  **步骤3**:
+    通过登录接口,获取一个Token，默认12小时失效<br>
+  **步骤4**:
+    根据步骤1中的表格，通过testController可以测试用户是否具有某个角色或者具有某个资源的权限<br>  
+## todo
+  目前表结构没有按照公司来划分、后续再添加      
--- doc/sql/auth.sql
+++ doc/sql/auth.sql
@@ -11,11 +11,28 @@
  Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 08/07/2020 16:57:13
+ Date: 09/07/2020 15:26:54
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_company
+-- ----------------------------
+DROP TABLE IF EXISTS `t_company`;
+CREATE TABLE `t_company`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '企业全称',
+  `short_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '企业简称',
+  `city` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '企业所在城市',
+  `contact` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '联系人',
+  `phone` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `creater_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `disabled` tinyint(1) UNSIGNED NULL DEFAULT 1 COMMENT '0 启用 1 禁用',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '企业信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_resource
@@ -26,7 +43,7 @@ CREATE TABLE `t_resource`  (
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '资源名称',
   `permission` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限标识',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '资源表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '资源表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_resource
@@ -47,7 +64,7 @@ CREATE TABLE `t_role`  (
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_role
@@ -66,7 +83,7 @@ CREATE TABLE `t_role_resource`  (
   `role_id` int(11) NULL DEFAULT NULL COMMENT '角色id',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_resource_id_role_id`(`resource_id`, `role_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色资源关系表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色资源关系表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_role_resource
@@ -95,7 +112,7 @@ CREATE TABLE `t_sys_token`  (
 -- ----------------------------
 -- Records of t_sys_token
 -- ----------------------------
-INSERT INTO `t_sys_token` VALUES (1, '2020-07-09 03:27:13', '784f2716eef5d6358a045d9749a05639', '2020-07-08 15:27:13');
+INSERT INTO `t_sys_token` VALUES (1, '2020-07-10 03:24:01', '197f92b9c3f65a55fc50c9cbd27dc9ff', '2020-07-09 15:24:01');
 
 -- ----------------------------
 -- Table structure for t_user
@@ -109,15 +126,20 @@ CREATE TABLE `t_user`  (
   `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
+  `user_type` int(1) NOT NULL COMMENT '账号类型 1主账号 2普通账号',
+  `company_id` int(11) NOT NULL COMMENT '公司id',
+  `phone` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `disabled` tinyint(1) UNSIGNED NULL DEFAULT 1 COMMENT '0 启用 1 禁用',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES (1, '123', '管理员', '王大陆', 'admin', NULL, NULL);
-INSERT INTO `t_user` VALUES (2, '123', '主管', '王大锤', 'jack', NULL, NULL);
-INSERT INTO `t_user` VALUES (3, '123', '销售经理', '玛丽', 'marry', NULL, NULL);
+INSERT INTO `t_user` VALUES (1, '123', '管理员', '王大陆', 'admin', NULL, NULL, 0, 0, NULL, NULL, 1);
+INSERT INTO `t_user` VALUES (2, '123', '主管', '王大锤', 'jack', NULL, NULL, 0, 0, NULL, NULL, 1);
+INSERT INTO `t_user` VALUES (3, '123', '销售经理', '玛丽', 'marry', NULL, NULL, 0, 0, NULL, NULL, 1);
 
 -- ----------------------------
 -- Table structure for t_user_role
@@ -129,7 +151,7 @@ CREATE TABLE `t_user_role`  (
   `user_id` int(11) NULL DEFAULT NULL COMMENT '用户id',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_role_id_user_id`(`role_id`, `user_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 54 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 57 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_user_role
